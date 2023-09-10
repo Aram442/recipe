b91c2d578e2378782b2f47111f8966d579cd2422
Aram A. Ahmad <aramaahmad42@gmail.com>
2023-09-10 21:00:22 +0300

    fetching API Successfuly

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useEffect } from "react";
 
 function Popular() {
-  return (
-    <div>Popular</div>
-  )
+  useEffect(() => {
+    //we should use useEffect to Load my API Every (reRenderd Project)
+    getPopular();
+  }, []);
+
+  const getPopular = async () => {
+    const api = await fetch(
+      `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
+    );
+    const data = await api.json();
+    // to get information about Our API work or not ==> console.log(data);
+    console.log(data);
+  };
+  return <div>Popular</div>;
 }
 
-export default Popular
+export default Popular;
