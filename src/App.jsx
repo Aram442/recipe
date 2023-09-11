908c610d5ec38a82cff8c7e0a5600efe174670ec
Aram A. Ahmad <aramaahmad42@gmail.com>
2023-09-11 13:33:30 +0300

    Adding NavLinks to Categore components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import Pages from "./pages/Pages";
 import Category from "./components/Category";
 import styled from "styled-components";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Category />
-      <Pages />
+      <BrowserRouter>
+        <Category />
+        <Pages />
+      </BrowserRouter>
     </div>
   );
 }
--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -1,16 +1,14 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import Cuisine from "./Cuisine";
 
 function Pages() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/cuisine" element={<Cuisine />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/cuisine/:type" element={<Cuisine />} />
+    </Routes>
   );
 }
 
