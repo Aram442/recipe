5e3ce1215d840ea3e505bedd6fb944f5b0df76b3
Aram A. Ahmad <aramaahmad42@gmail.com>
2023-09-11 13:23:52 +0300

    Creating Cuisine & Routers in Pages Components

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -1,11 +1,16 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Home";
+import Cuisine from "./Cuisine";
 
 function Pages() {
   return (
-    <div>
-      <Home />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cuisine" element={<Cuisine />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
