c47753ff2e178ad16ac10cbcd27854b12b0e09b8
Aram A. Ahmad <aramaahmad42@gmail.com>
2023-09-11 13:15:24 +0300

    Creating Category and seting up their style

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import Pages from "./pages/Pages";
+import Category from "./components/Category";
+import styled from "styled-components";
 
 function App() {
   return (
     <div className="App">
+      <Category />
       <Pages />
     </div>
   );
