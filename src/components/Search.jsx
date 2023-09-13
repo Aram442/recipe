af7ecb456fdf91ca11a0ccea97630cadb5e019ac
Aram A. Ahmad <aramaahmad42@gmail.com>
2023-09-13 17:31:20 +0300

    adding Mobile Responsive for Search

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import { FaSearch } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
 
@@ -50,6 +50,31 @@ const FormStyle = styled.form`
     left: 0%;
     transform: translate(100%, -50%);
     color: white;
+  }
+
+  @media (max-width: 768px) {
+    width: 100%;
+    position: relative;
+
+    input {
+      border: none;
+      background: linear-gradient(35deg, #494949, #313131);
+      font-size: 0.8rem;
+      color: white;
+      padding: 0.5rem 3rem;
+      border: none;
+      border-radius: 0.3rem;
+      outline: none;
+      width: 100%;
+    }
+    svg {
+      font-size: 0.8rem;
+      position: absolute;
+      top: 50%;
+      left: 0%;
+      transform: translate(100%, -50%);
+      color: white;
+    }
   }
 `;
 
