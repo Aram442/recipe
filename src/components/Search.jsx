214eef85c98b651581205fa6c19a76311d9fe41e
Aram A. Ahmad <aramaahmad42@gmail.com>
2023-09-13 16:41:00 +0300

    update Search bar

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -27,8 +27,6 @@ function Search() {
 }
 
 const FormStyle = styled.form`
-  margin: 0rem 20rem;
-
   div {
     width: 100%;
     position: relative;
@@ -37,11 +35,11 @@ const FormStyle = styled.form`
   input {
     border: none;
     background: linear-gradient(35deg, #494949, #313131);
-    font-size: 1.5rem;
+    font-size: 1rem;
     color: white;
     padding: 1rem 3rem;
     border: none;
-    border-radius: 1rem;
+    border-radius: 0.6rem;
     outline: none;
     width: 100%;
   }
