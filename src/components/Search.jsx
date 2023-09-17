bd98779276d50346b3ff4acd6ff588de2e37d675
Aram A. Ahmad <aramaahmad42@gmail.com>
2023-09-17 10:50:10 +0300

    fixing Search Zoom

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -60,23 +60,14 @@ const FormStyle = styled.form`
     position: relative;
 
     input {
-      border: none;
-      background: linear-gradient(35deg, #494949, #313131);
       font-size: 0.8rem;
-      color: white;
       padding: 0.5rem 3rem;
-      border: none;
       border-radius: 0.3rem;
-      outline: none;
-      width: 100%;
+      touchaction: "manipulation"; // Disable zoom on touch devices
     }
     svg {
       font-size: 0.8rem;
-      position: absolute;
-      top: 50%;
-      left: 0%;
       transform: translate(100%, -50%);
-      color: white;
     }
   }
 `;
